8aabf35402dc00311d4a4a949b85225cb630c38e
James Laverack <james@jameslaverack.com>
2020-04-03 04:06:05 +0100

    save-all

--- src/bin/management_api.rs
+++ src/bin/management_api.rs
@@ -16,7 +16,7 @@ use tokio::time::{self, Duration};
 use tonic::{transport::Server, Request, Response, Status};
 
 use management::minecraft_management_server::{MinecraftManagementServer, MinecraftManagement};
-use management::{ListPlayersRequest, ListPlayersReply, Player};
+use management::{ListPlayersRequest, ListPlayersReply, Player, SaveAllRequest, SaveAllReply};
 
 pub mod management {
     tonic::include_proto!("management");
@@ -30,6 +30,67 @@ pub struct DummyMinecraftManagement {
 
 #[tonic::async_trait]
 impl MinecraftManagement for DummyMinecraftManagement {
+    async fn save_all(
+        &self,
+        _request: Request<SaveAllRequest>,
+    ) -> Result<Response<SaveAllReply>, Status> {
+        info!("Got a request to Save All");
+        // Register to logs
+        let logs = &mut self.logs.lock().await.subscribe();
+        // Send the save-all command
+        match self.input.lock().await.clone().send("save-all".to_string()).await {
+            Ok(_) => (),
+            Err(e) => {
+                error!("Error sending save-all command: {}", e);
+                return Err(Status::unavailable("Failed to communicate with Minecraft process."));
+            },
+        }
+        // Parse response, waiting for up to half a second
+        let delay_millis = 500;
+        let mut delay = time::delay_for(Duration::from_millis(delay_millis));
+
+        let start_save_regex = Regex::new(r"^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]: Saving the game \(this may take a moment!\)$").unwrap();
+        let end_save_regex = Regex::new(r"^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]: Saved the game").unwrap();
+
+
+        // Give it a short timeout to start the save, and a long timeout to finish it.
+        loop {
+            tokio::select! {
+                _ = &mut delay => {
+                    error!("operation timed out");
+                    return Err(Status::deadline_exceeded(format!("Minecraft did not respond in under {}ms", delay_millis)));
+                },
+                Ok(line) = logs.recv() => {
+                    debug!("Save all task, analysing log line: {}", line);
+                    if start_save_regex.is_match(&line) {
+                        debug!("Minecraft declared start of save.");
+                        break;
+                    }
+                },
+            }
+        }
+
+        let full_delay_seconds = 5;
+        let mut full_delay = time::delay_for(Duration::from_secs(full_delay_seconds));
+        // Second, longer timeout for the full save
+        loop {
+            tokio::select! {
+                _ = &mut full_delay => {
+                    error!("operation timed out");
+                    return Err(Status::deadline_exceeded(format!("Minecraft did not complete save in under {} seconds", full_delay_seconds)));
+                },
+                Ok(line) = logs.recv() => {
+                    debug!("Save all task, analysing log line: {}", line);
+                    if end_save_regex.is_match(&line) {
+                        debug!("Minecraft declared end of save.");
+                        return Ok(Response::new(SaveAllReply{}));
+                    }
+                },
+            }
+        }
+
+    }
+
     async fn list_players(
         &self,
         _request: Request<ListPlayersRequest>,
