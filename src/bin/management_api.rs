e054684a3989cdf3448e4fcc33df5d94804bd780
James Laverack <james@jameslaverack.com>
2020-04-11 23:33:33 +0100

    Implement all saving rpcs

--- src/bin/management_api.rs
+++ src/bin/management_api.rs
@@ -45,158 +45,20 @@ impl RconMinecraftManagement {
 
 #[tonic::async_trait]
 impl MinecraftManagement for RconMinecraftManagement {
-    /*
-    async fn disable_automatic_save(
-        &self,
-        _request: Request<DisableAutomaticSaveRequest>,
-    ) -> Result<Response<DisableAutomaticSaveReply>, Status> {
-        info!("Got a request to disable automatic save");
-        // Register to logs
-        let logs = &mut self.logs.lock().await.subscribe();
-        // Send the save-all command
-        match self.input.lock().await.clone().send("save-off".to_string()).await {
-            Ok(_) => (),
-            Err(e) => {
-                error!("Error sending save-off command: {}", e);
-                return Err(Status::unavailable("Failed to communicate with Minecraft process."));
-            },
-        }
-        // Parse response, waiting for up to half a second
-        let delay_millis = 500;
-        let mut delay = time::delay_for(Duration::from_millis(delay_millis));
 
-        let save_off_regex = Regex::new(r"^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]: (Automatic saving is now disabled|Saving is already turned off)$").unwrap();
-
-        // Give it a short timeout to start the save, and a long timeout to finish it.
-        loop {
-            tokio::select! {
-                _ = &mut delay => {
-                    error!("operation timed out");
-                    return Err(Status::deadline_exceeded(format!("Minecraft did not respond in under {}ms", delay_millis)));
-                },
-                Ok(line) = logs.recv() => {
-                    debug!("Save all task, analysing log line: {}", line);
-                    if save_off_regex.is_match(&line) {
-                        debug!("Minecraft declared save-off.");
-                        return Ok(Response::new(DisableAutomaticSaveReply{}));
-                    }
-                },
-            }
-        }
-    }
-
-    async fn enable_automatic_save(
-        &self,
-        _request: Request<EnableAutomaticSaveRequest>,
-    ) -> Result<Response<EnableAutomaticSaveReply>, Status> {
-        info!("Got a request to enable automatic save");
-        // Register to logs
-        let logs = &mut self.logs.lock().await.subscribe();
-        // Send the save-all command
-        match self.input.lock().await.clone().send("save-on".to_string()).await {
-            Ok(_) => (),
-            Err(e) => {
-                error!("Error sending save-on command: {}", e);
-                return Err(Status::unavailable("Failed to communicate with Minecraft process."));
-            },
-        }
-        // Parse response, waiting for up to half a second
-        let delay_millis = 500;
-        let mut delay = time::delay_for(Duration::from_millis(delay_millis));
-
-        let save_off_regex = Regex::new(r"^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]: (Automatic saving is now enabled|Saving is already turned on)$").unwrap();
-
-        // Give it a short timeout to start the save, and a long timeout to finish it.
-        loop {
-            tokio::select! {
-                _ = &mut delay => {
-                    error!("operation timed out");
-                    return Err(Status::deadline_exceeded(format!("Minecraft did not respond in under {}ms", delay_millis)));
-                },
-                Ok(line) = logs.recv() => {
-                    debug!("Save all task, analysing log line: {}", line);
-                    if save_off_regex.is_match(&line) {
-                        debug!("Minecraft declared save-off.");
-                        return Ok(Response::new(EnableAutomaticSaveReply{}));
-                    }
-                },
-            }
-        }
-    }
-
-    async fn save_all(
-        &self,
-        _request: Request<SaveAllRequest>,
-    ) -> Result<Response<SaveAllReply>, Status> {
-        info!("Got a request to Save All");
-        // Register to logs
-        let logs = &mut self.logs.lock().await.subscribe();
-        // Send the save-all command
-        match self.input.lock().await.clone().send("save-all".to_string()).await {
-            Ok(_) => (),
-            Err(e) => {
-                error!("Error sending save-all command: {}", e);
-                return Err(Status::unavailable("Failed to communicate with Minecraft process."));
-            },
-        }
-        // Parse response, waiting for up to half a second
-        let delay_millis = 500;
-        let mut delay = time::delay_for(Duration::from_millis(delay_millis));
-
-        let start_save_regex = Regex::new(r"^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]: Saving the game \(this may take a moment!\)$").unwrap();
-        let end_save_regex = Regex::new(r"^\[\d\d:\d\d:\d\d\] \[Server thread/INFO\]: Saved the game").unwrap();
-
-
-        // Give it a short timeout to start the save, and a long timeout to finish it.
-        loop {
-            tokio::select! {
-                _ = &mut delay => {
-                    error!("operation timed out");
-                    return Err(Status::deadline_exceeded(format!("Minecraft did not respond in under {}ms", delay_millis)));
-                },
-                Ok(line) = logs.recv() => {
-                    debug!("Save all task, analysing log line: {}", line);
-                    if start_save_regex.is_match(&line) {
-                        debug!("Minecraft declared start of save.");
-                        break;
-                    }
-                },
-            }
-        }
-
-        let full_delay_seconds = 5;
-        let mut full_delay = time::delay_for(Duration::from_secs(full_delay_seconds));
-        // Second, longer timeout for the full save
-        loop {
-            tokio::select! {
-                _ = &mut full_delay => {
-                    error!("operation timed out");
-                    return Err(Status::deadline_exceeded(format!("Minecraft did not complete save in under {} seconds", full_delay_seconds)));
-                },
-                Ok(line) = logs.recv() => {
-                    debug!("Save all task, analysing log line: {}", line);
-                    if end_save_regex.is_match(&line) {
-                        debug!("Minecraft declared end of save.");
-                        return Ok(Response::new(SaveAllReply{}));
-                    }
-                },
-            }
-        }
-
-    }
-    */
     async fn list_players(
         &self,
         _request: Request<()>,
     ) -> Result<Response<ListPlayersReply>, Status> {
         info!("Got a request to list players");
+        const CMD: &str = "save-all";
         let mut conn = convert_conn_err(RconConnection::connect(self.rcon_address, self.rcon_password.as_str()).await)?;
-        let response = convert_conn_err(conn.cmd("list uuids").await)?;
+        let response = convert_conn_err(conn.cmd(CMD).await)?;
 
         let player_list_regex = Regex::new(r"^There are (?P<current>\d+) of a max (?P<max>\d+) players online:").unwrap();
         let player_details_regex = Regex::new(r"(?P<name>\w+)[ ]\((?P<uuid>[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})\)").unwrap();
 
-        debug!("Got response {}", response);
+        debug!("Got response from {}: {}", CMD, response);
 
         return match player_list_regex.captures(&response) {
             Some(caps) => {
@@ -218,10 +80,73 @@ impl MinecraftManagement for RconMinecraftManagement {
                 }))
             },
             None => {
-                error!("Response from a 'list uuids' did not match expectation!");
+                error!("Response from a '{}' did not match expectation!", CMD);
                 Err(Status::unknown("Minecraft responded in an unexpected way"))
             },
         };
+    }
+
+    async fn enable_automatic_save(
+        &self,
+        _request: Request<()>,
+    ) -> Result<Response<()>, Status> {
+        debug!("Got a request to enable automatic saving");
+        const CMD: &str = "save-on";
+        let mut conn = convert_conn_err(RconConnection::connect(self.rcon_address, self.rcon_password.as_str()).await)?;
+        let response = convert_conn_err(conn.cmd(CMD).await)?;
+
+        let save_on_regex = Regex::new(r"(Automatic saving is now enabled|Saving is already turned on)").unwrap();
+
+        debug!("Got response from {}: {}", CMD, response);
+
+        if save_on_regex.is_match(&response) {
+            Ok(Response::new(()))
+        } else {
+            error!("Response from a '{}' did not match expectation!", CMD);
+            Err(Status::unknown("Minecraft responded in an unexpected way"))
+        }
+    }
+
+    async fn disable_automatic_save(
+        &self,
+        _request: Request<()>,
+    ) -> Result<Response<()>, Status> {
+        debug!("Got a request to disable automatic saving");
+        const CMD: &str = "save-off";
+        let mut conn = convert_conn_err(RconConnection::connect(self.rcon_address, self.rcon_password.as_str()).await)?;
+        let response = convert_conn_err(conn.cmd(CMD).await)?;
+
+        let save_off_regex = Regex::new(r"(Automatic saving is now disabled|Saving is already turned off)").unwrap();
+
+        debug!("Got response from {}: {}", CMD, response);
+
+        if save_off_regex.is_match(&response) {
+            Ok(Response::new(()))
+        } else {
+            error!("Response from a '{}' did not match expectation!", CMD);
+            Err(Status::unknown("Minecraft responded in an unexpected way"))
+        }
+    }
+
+    async fn save_all(
+        &self,
+        _request: Request<()>,
+    ) -> Result<Response<()>, Status> {
+        debug!("Got a request to save all");
+        const CMD: &str = "save-all";
+        let mut conn = convert_conn_err(RconConnection::connect(self.rcon_address, self.rcon_password.as_str()).await)?;
+        let response = convert_conn_err(conn.cmd(CMD).await)?;
+
+        let saved_game_regex = Regex::new(r"Saved the game").unwrap();
+
+        debug!("Got response from {}: {}", CMD, response);
+
+        if saved_game_regex.is_match(&response) {
+            Ok(Response::new(()))
+        } else {
+            error!("Response from a '{}' did not match expectation!", CMD);
+            Err(Status::unknown("Minecraft responded in an unexpected way"))
+        }
     }
 }
 
